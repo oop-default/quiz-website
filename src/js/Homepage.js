ad5e676ecd725c0f57f308fe8473a69c7f1684c9
jikksi <48617867+jikksi@users.noreply.github.com>
2019-06-16 16:45:00 +0400

    Web file

---
+++ src/js/Homepage.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Register from './Register';
+import Login from './Login';
+import '../css/Homepage.css';
+
+class Homepage extends Component {
+    constructor() {
+        super();
+        console.log("Homepage");
+    }
+    componentDidMount() {
+        console.log("componentDidMount");
+        console.log("check for users login, if user is already logged in send to Acount  page,else start login page");
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log("componentDidUpdate");
+
+    }
+    render() {
+        return (<div>
+            <ul>
+                <li id="siteName">QuizSite</li>
+                <li id="LoginTable">
+                    <Login />
+                </li>
+            </ul>
+            <div id="register"><Register /></div>
+        </div>
+
+        );
+    }
+}
+
+export default Homepage;
