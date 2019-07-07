4a5c4bc768a762975f10d7027dc34dd9aba68414
fnrngg <balsh17@freeuni.edu.ge>
2019-07-07 18:23:36 +0400

    base

--- Api/Api/src/database/DatabaseManager.java
+++ Api/Api/src/database/DatabaseManager.java
@@ -2,9 +2,13 @@ package database;
 
 
 import com.mysql.jdbc.ResultSetMetaData;
+import responseModels.friendsQuizzesResponse;
+import responseModels.quizzesResponse;
 
 import javax.swing.plaf.nimbus.State;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DatabaseManager {
     static  String account = MyDBInfo.MYSQL_USERNAME;
@@ -42,6 +46,99 @@ public class DatabaseManager {
 
         return null;
     }
+
+    public List<quizzesResponse> getCreatedQuizzes(int id) {
+        List<quizzesResponse> quizzes = new ArrayList<>();
+//        PreparedStatement stmt;
+//        try {
+//            stmt = con.prepareStatement("select * from quizzes where author_id = ?");
+//            stmt.setInt(1, id);
+//
+//            rs = stmt.executeQuery();
+//            while(rs.next()) {
+//                Quiz quiz = new Quiz();
+//                quiz.setId(rs.getInt(1));
+//                quiz.setTitle(rs.getString(2));
+//                quizzes.add(quiz);
+//            }
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+        quizzesResponse quiz = new quizzesResponse(1, "Sad");
+        quizzes.add(quiz);
+
+        return quizzes;
+    }
+
+    public List<quizzesResponse> getTakenQuizzes(int id) {
+        List<quizzesResponse> quizzes = new ArrayList<>();
+//        PreparedStatement stmt;
+//        try {
+//            stmt = con.prepareStatement("select * from history where account_id = ?");
+//            stmt.setInt(1, id);
+//
+//            rs = stmt.executeQuery();
+//            while(rs.next()) {
+//                quizzesResponse quiz = new quizzesResponse(rs.getInt(1), rs.getString(2));
+//                quizzes.add(quiz);
+//            }
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+        quizzesResponse quiz = new quizzesResponse(1, "Sad");
+        quizzes.add(quiz);
+
+        return quizzes;
+    }
+
+    public List<friendsQuizzesResponse> getFriendsQuizActivity(int id) {
+        List<friendsQuizzesResponse> quizzes = new ArrayList<>();
+
+//        PreparedStatement stmt;
+//        try {
+//            String sql1 = "select f.id, a.username, q.id, q.title, h.num_points " +
+//                    "from history h " +
+//                    "join quizzes q " +
+//                    "on q.id = h.quiz_id " +
+//                    "join friends f " +
+//                    "on f.id = h.account_id " +
+//                    "join accounts a " +
+//                    "on a.id = f.second_id " +
+//                    "where h.account_id in " +
+//                    "(select second_id from friends where first_id=?) " +
+//                    "order by h.date_taken " +
+//                    "union" +
+//                    "select f.id, a.username, q.id, q.title, h.num_points " +
+//                    "join history h " +
+//                    "on q.id = h.quiz_id " +
+//                    "join friends f " +
+//                    "on f.id = h.account_id " +
+//                    "join accounts a " +
+//                    "on a.id = f.first_id" +
+//                    "where h.account_id in " +
+//                    "(select first_id from friends where second_id=?) " +
+//                    "order by h.date_taken";
+//            stmt = con.prepareStatement(sql1);
+//            stmt.setInt(1, id);
+//
+//            rs = stmt.executeQuery();
+//            while(rs.next()) {
+//                friendsQuizzesResponse quiz = new friendsQuizzesResponse(rs.getInt(1),
+//                        rs.getString(2), rs.getInt(3), rs.getString(4), rs.getInt(5));
+//                quizzes.add(quiz);
+//            }
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+        friendsQuizzesResponse quiz = new friendsQuizzesResponse(1, "vigaca", 1, "ragaca", 100);
+        quizzes.add(quiz);
+
+        return quizzes;
+    }
+
     public Connection getConnection(){
         return con;
     }
