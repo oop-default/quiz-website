503874f9c6a31a534dd26db31f95db9065e4de76
zvikiNozadze <znoza17@freeuni.edu.ge>
2019-07-14 21:06:48 +0400

    create quiz button added

--- react/quizes/src/homepage/js/NavBar.js
+++ react/quizes/src/homepage/js/NavBar.js
@@ -4,12 +4,20 @@ import Notifications from './Notifications';
 
 
 class NavBar extends Component{
+    // http://localhost:3000/createQuiz
+    componentDidMount() {
+      document.getElementById("create").onclick = function () {
+        window.location.href = "http://localhost:3000/createQuiz";
+      };
+    }
+
     render() {
         return(
             <nav className = "navBar">
                 <div className = "logo">
                     <a href = "http://localhost:3000/" style = {{textDecoration:"none", color:"white"}}>Quizify</a>
                 </div>
+                <button id="create" className="createNewQuiz">Create New Quiz</button>
                 <div className = "search">
                     <input type = "text" name = "userName" placeholder = "Search for someone"
                         style={{ minWidth: "50%", width: "200%" }}></input>
